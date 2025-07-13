b7bccd01230827602bb05909b5efe77480beb6bf
Deepak <deepak2603dk@gmail.com>
2025-07-14 01:39:02 +0530

    fix3

--- src/app/_components/ui/common/navbar/Avatar.tsx
+++ src/app/_components/ui/common/navbar/Avatar.tsx
@@ -1,7 +1,6 @@
-import { AvatarType } from "@/types/types";
 import React from "react";
 
-export default function Avatar({ avatar }: AvatarType) {
+export default function Avatar({ avatar }:{avatar:string}) {
   return (
     <div className="h-10 aspect-square rounded-full bg-white/50 flex justify-center items-center border-t-2 border-t-white/50">
       <p className="font-bold">{avatar[0].toUpperCase()}</p>
---
+++ src/lib/types/type.ts
@@ -0,0 +1,9 @@
+export interface UserType {
+  id: string
+  email: string
+  name: string
+  image: string | null
+  emailVerified: boolean
+  createdAt: string
+  updatedAt: string
+}
