c5969ab02422eea1d1d52f2c17b2fcfed17bf5b8
Deepak <deepak2603dk@gmail.com>
2025-07-13 22:08:50 +0530

    fiix imports

--- src/app/(user)/publish/layout.tsx
+++ src/app/(user)/publish/layout.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { FormProvider } from "./FormContext";
-import Image from "next/image";
 import ScreenHandlers from "@/app/_components/wrappers/ScreenHandlers";
 import PaddingWrapper from "@/app/_components/wrappers/PaddingWrapper";
-import { grainyBg } from "../../../../lib/constants";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
